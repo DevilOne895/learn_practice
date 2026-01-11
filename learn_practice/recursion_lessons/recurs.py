d875b2ca693f34e7433f4bde9d0fbbcf6f0f5091
Eugene Bogatynskii <devilone895@gmail.com>
2026-01-11 19:18:36 +0200

    add new tasks

--- learn_practice/recursion_lessons/recurs.py
+++ learn_practice/recursion_lessons/recurs.py
@@ -26,4 +26,38 @@ def digits(n):
         return 1 + digits(n // 10)
 
 
-print(digits(12345))
+#print(digits(12345))
+
+
+#_______________________SECOND-LEVEL_____________________________________
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+#print(factorial(5))
+
+
+def pow(a,n):
+    if n == 1:
+        return 1
+    return a * pow(a,n - 1)
+
+#print(pow(2,4))
+
+def sumDigits(n):
+    if n == 0:
+        return 0
+    return sumDigits(n // 10) + n % 10
+
+#print(sumDigits(123))
+
+def degree(n):
+    if n == 1:
+        return True
+    if n <= 0 or n % 2 != 0:
+        return False
+    return degree(n // 2 )
+
+print(degree(5))
